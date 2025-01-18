1ede67fe23bfceda5c75f9b0b22b2710e0d35520
asrar211 <113281948+asrar211@users.noreply.github.com>
2025-01-18 13:13:15 +0530

    Create CovertArrayToLL.java

---
+++ LinkedList/CovertArrayToLL.java
@@ -0,0 +1,31 @@
+class Node{
+    int data;
+    Node next;
+    Node(int data, Node next){
+        this.data = data;
+        this.next = next;
+    }
+    Node(int data1){
+        this.data = data1;
+        this.next = null;
+    }
+}
+public class CovertArrayToLL {
+    public static Node convert(int[] arr){
+        Node head = new Node(arr[0]);
+        Node mover = head;
+
+        for(int i=0; i< arr.length; i++){
+            Node temp = new Node(arr[i]);
+            mover.next = temp;
+            mover = temp;
+        }
+        return head;
+    }
+
+    public static void main(String[] args){
+        int[] arr= {2, 3, 4, 5};
+        Node head = convert(arr);
+        System.out.println(head.data);
+    }
+}
