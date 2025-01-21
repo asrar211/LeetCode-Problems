4b19bd8598bdf23c038a6dba25e436f67c5e9d4f
asrar211 <113281948+asrar211@users.noreply.github.com>
2025-01-21 18:46:45 +0530

    Create DoublyTraversal.java

---
+++ LinkedList/DoublyTraversal.java
@@ -0,0 +1,19 @@
+class Node{
+    int data;
+    Node prev;
+    Node next;
+
+    public Node(int data){
+        this.data= data;
+        this.prev = null;
+        this.next = null;
+    }
+}
+class DoublyLinkedList{
+    private Node head;
+
+    
+}
+public class DoublyTraversal {
+    
+}
