932101cda4642a9286c6998a8c64fb2ed2669cfd
asrar211 <113281948+asrar211@users.noreply.github.com>
2025-01-22 12:43:45 +0530

    Update DeletionDLL.java

--- LinkedList/DeletionDLL.java
+++ LinkedList/DeletionDLL.java
@@ -48,6 +48,61 @@ class DLL {
         temp.prev = null;
     }
 
+    public void deleteAtK(int k){
+        if(head == null){
+            return;
+        }
+        int count =1;
+        Node temp = head;
+
+        while (temp != null && count < k) {
+            temp = temp.next;
+            count++;
+        }
+
+        if (temp == null) {
+            return;
+        }
+
+        Node back = temp.prev;
+        Node front = temp.next;
+
+        if(back == null && front == null){
+            head = null;
+        }
+        else if(back == null){
+            deleteAtHead();
+        } else if( front == null){
+            deleteAtTail();
+        } 
+        back.next = front;
+        front.prev = back;
+    }
+
+    public void deleteByVal(int val){
+        Node temp = head;
+
+        while(temp != null && temp.data != val){
+            temp = temp.next;
+        }
+        if(temp == null){
+            return;
+        }
+
+        Node back = temp.prev;
+        Node front = temp.next;
+        if(back == null && front == null){
+            head = null;
+        }
+        else if(back == null){
+            deleteAtHead();
+        } else if( front == null){
+            deleteAtTail();
+        }
+        back.next = front;
+        front.prev = back;
+    }
+
 }
 
 
