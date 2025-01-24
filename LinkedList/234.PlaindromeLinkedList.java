bc6c3276ee333a7e5e8b04e7d27964bc473d7fc6
asrar211 <113281948+asrar211@users.noreply.github.com>
2025-01-24 16:48:05 +0530

    Create 234.PlaindromeLinkedList.java

---
+++ LinkedList/234.PlaindromeLinkedList.java
@@ -0,0 +1,43 @@
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if(head == null || head.next == null){
+            return true;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode secondHalf = reverseList(slow);
+
+        ListNode firstHalf = head;
+        ListNode secondHalfCopy = secondHalf;
+
+        while(secondHalf != null){
+            if(firstHalf.val != secondHalf.val){
+                return false;
+            }
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+
+        reverseList(secondHalfCopy);
+
+        return true;
+    }
+
+    private ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode nextNode = head.next;
+            head.next = prev;
+            prev = head;
+            head = nextNode;
+        }
+        return prev;
+    }
+}
