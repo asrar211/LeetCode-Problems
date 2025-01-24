4c708e9d05f9e1fe738bc66f03924b6fb416e45e
asrar211 <113281948+asrar211@users.noreply.github.com>
2025-01-24 16:48:07 +0530

    Update 206.ReversedList.java

--- LinkedList/206.ReversedList.java
+++ LinkedList/206.ReversedList.java
@@ -1,14 +1,30 @@
+//Iterative
 class Solution {
     public ListNode reverseList(ListNode head) {
         ListNode current = head;
         ListNode prev= null;
 
         while(current != null){
-            ListNode temp = current.next;
+            ListNode front = current.next;
             current.next = prev;
             prev= current;
-            current= temp;
+            current= front;
         }
         return prev;
+    }
+}
+
+//Recursive
+class Solution{
+    public ListNode reverseList(ListNode head){
+        if(head == null || head.next == null){
+            return head;
+        }
+
+        ListNode newHead = reverseList(head.next);
+        ListNode front = head.next;
+        front.next = head;
+        head.next = null;
+        return newHead;
     }
 }
