75ff48d23bf56b9837b1bbc020682bda6588e082
asrar211 <113281948+asrar211@users.noreply.github.com>
2025-02-08 13:48:19 +0530

    Create 402.RemoveKDigits.java

---
+++ Stack&Queue/402.RemoveKDigits.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String removeKdigits(String num, int k) {
+        Stack<Character> st = new Stack<>();
+
+        for (int i = 0; i < num.length(); i++) {
+            while (!st.isEmpty() && k > 0 && st.peek() > num.charAt(i)) {
+                st.pop();
+                k--;
+            }
+            st.push(num.charAt(i));
+        }
+
+        while (k > 0 && !st.isEmpty()) {
+            st.pop();
+            k--;
+        }
+
+        StringBuilder result = new StringBuilder();
+        while (!st.isEmpty()) {
+            result.append(st.pop());
+        }
+
+        result.reverse();
+
+        while (result.length() > 0 && result.charAt(0) == '0') {
+            result.deleteCharAt(0);
+        }
+
+        return result.length() == 0 ? "0" : result.toString();
+
+    }
+}
