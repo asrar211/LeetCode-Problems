3c5d6dfa4e904de5c0ba1a2c1766b83686a06d13
asrar211 <113281948+asrar211@users.noreply.github.com>
2025-02-18 12:43:53 +0530

    Create BinaryTree.java

---
+++ Trees/BinaryTree.java
@@ -0,0 +1,19 @@
+package Trees;
+
+class TreeNode{
+    int data;
+    TreeNode left, right;
+
+    public TreeNode(int data) {
+        this.data = data;
+        this.left = this.right = null;
+    }
+}
+
+public class BinaryTree {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+    }
+}
